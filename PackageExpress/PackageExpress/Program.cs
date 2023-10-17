e8de43b7e7de44edf8d0e4e4a11397cf674c95ce
meti1276 <134025235+meti1276@users.noreply.github.com>
2023-10-17 16:12:33 -0400

    update

---
+++ PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PackageExpress
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express!");
+            Console.WriteLine("Please enter weight");
+            int weight=Convert.ToInt32(Console.ReadLine());
+            if (weight >50 )
+            {
+             Console.WriteLine("Package too heavy to be shipped via Package Express.Have a good day.");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
